c7fdc4d79044629f45da7c61f1f94b5d0f19d0fe
ImoucheG <imouche.guillaume@gmail.com>
2016-04-03 17:52:08 +0200

    Front : Display Tags: OK, Display counter by tags TODO, resources, autocomplete TODO too

---
+++ front-end/RedisKD/app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap}    from 'angular2/platform/browser';
+import {Tag} from './app.tags';
+
+bootstrap(Tag);
--- services/typeaheadService.js
+++ services/typeaheadService.js
@@ -3,7 +3,7 @@ var db = require('./db/db');
 var typeaheadService = {
 
 	get: function(prefix, callback){
-		return db.getAll(prefix, callback);
+		return db.getAll(prefix,'keys', callback);
 	}
 };
 
